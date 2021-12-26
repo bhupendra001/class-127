3d09a98b02272ca8ee7de1624152343b0ad52dc3
bhupendra001 <63101397+bhupendra001@users.noreply.github.com>
2021-12-26 20:34:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftwrist = 0;
+rightwrist = 0;
+
+
+function preload(){
+
+}
+
+function draw(){
+ 
+document.getElementById("square_size").innerHTML = "width and height of a square will be = " + difference + " px";   
+fill("cyan");   
+stroke("cyan");
+square(noseX,noseY,difference);
+
+}
+
+function setup(){
+
+    video = createCapture(VIDEO);  
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    
+    
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on("pose",gotposes);
+
+}
+
+function modelloaded(){
+
+console.log("poseNet is initialized")
+
+
+}
+
+function gotposes(results){
+
+if(results.length > 0){
+
+console.log(results);
+
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+
+console.log("noseX = " + noseX + "noseY" + noseY);
+leftwrist = results[0].pose.leftWrist.x;
+rightwrist = results[0].pose.rightWrist.x;
+difference = floor(leftwrist-rightwrist);
+
+console.log("leftwrist = " + leftwrist + "rightwrist" + rightwrist + "difference = " + difference);
+
+}
+
+}
